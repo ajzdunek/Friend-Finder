2516e6ba85362946484644bdc99eabcbc0d772d8
Andrew J. Zdunek <andrewjzdunek@gmail.com>
2018-02-23 11:00:34 -0600

    fixing up api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,11 @@ var path = require("path");
 var musicians = require("../data/friend.js");
 
 module.exports = function(app) {
-  app.get("/api/friend", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(musicians);
   })
 
-	 app.post("/api/friend", function(req, res) {
+	 app.post("/api/friends", function(req, res) {
 
 			var leadScore = req.body.scores;
 			var scoreArray = [];
